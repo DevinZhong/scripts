56e87b4cc9b51ed70b9807dc7a3c4baf58e35ba2
Devin Zhong <devinzhong@outlook.com>
2017-02-03 23:55:57 +0800

    update shadowsocks-libev_debian.sh

--- shadowsocks/shadowsocks-libev_debian.sh
+++ shadowsocks/shadowsocks-libev_debian.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
+#==================================================================
+#   System Required:  Debian 8 (32bit/64bit)
+#   Description:  Install Shadowsocks-libev for Debian 8
+#   Author: Devin Zhong <devinzhong@outlook.com>
+#   Intro:  https://github.com/DevinZhong/scripts/tree/master/shadowsocks
+#==================================================================
+
+# 清屏与打印脚本说明
+clear
+cat << EOF
+#############################################################
+# Install Shadowsocks-libev for Debian 8 (32bit/64bit)
+# Intro:  https://github.com/DevinZhong/scripts/tree/master/shadowsocks
+#
+# Author: Devin Zhong <devinzhong@outlook.com>
+#############################################################
+
+EOF
 
 # 确保是 root 来运行此脚本
 if [[ $EUID -ne 0 ]]; then
     echo "this script must be run as root!" 1>&2
+    exit 1
+fi
+
+# 确保系统为 Debian 发行版
+if [ ! -f /etc/debian_version ]; then
+    echo "distribution is not supported!" 1>&2
     exit 1
 fi
 
@@ -16,24 +40,31 @@ apt-get -y install git > /dev/null
 
 # 安装 shadowsocks-libev
 echo 'install shadowsocks-libev...'
+TEMP_DIR="shadowsocks-libev-$RANDOM"
+mkdir $TEMP_DIR && cd $TEMP_DIR
 git clone https://github.com/shadowsocks/shadowsocks-libev.git &> /dev/null
 cd shadowsocks-libev
-apt-get -y install --no-install-recommends build-essential autoconf libtool libssl-dev \
-    gawk debhelper dh-systemd init-system-helpers pkg-config asciidoc xmlto apg libpcre3-dev zlib1g-dev &> /dev/null
-dpkg-buildpackage -b -us -uc -i &> /dev/null
+apt-get -y install --no-install-recommends gettext build-essential autoconf libtool \
+    gawk debhelper dh-systemd init-system-helpers pkg-config asciidoc xmlto apg libpcre3-dev libmbedtls-dev \
+    libev-dev libudns-dev libsodium-dev &> /dev/null
+./autogen.sh && dpkg-buildpackage -b -us -uc -i &> /dev/null
 cd ..
 dpkg -i shadowsocks-libev*.deb &> /dev/null
-rm -rf shadowsocks-libev*
+cd ..
+rm -rf $TEMP_DIR
 
 # 配置 shadowsocks-libev
 echo 'configure shadowsocks-libev...'
+# 启用 UDP 转发、一次性认证、TCP 快速启动
 sed -i 's/\(DAEMON_ARGS\).*$/\1="-u -A --fast-open"/' /etc/default/shadowsocks-libev
 
 # 配置 config.json
-read -p "please input password for shadowsocks-libev (way2hacker.com):" shadowsockspwd
+echo "please input password (way2hacker.com):"
+read shadowsockspwd
 [ -z "${shadowsockspwd}" ] && shadowsockspwd="way2hacker.com"
 
-read -p "please input port for shadowsocks-libev (8686):" shadowsocksport
+echo "please input port (8686):"
+read shadowsocksport
 [ -z "$shadowsocksport" ] && shadowsocksport="8686"
 
 cat << EOF > /etc/shadowsocks-libev/config.json
@@ -63,7 +94,7 @@ cat << EOF
 
 please configure you shadowsocks client as below:
 ----------------------------------------
-server:"$(curl ifconfig.io 2>/dev/null)"
+server:"$(curl ifconfig.co 2>/dev/null)"
 server_port:${shadowsocksport}
 password:"${shadowsockspwd}"
 method:"chacha20"
