602610c25b19a045ce261dee106d6204f26deff8
Devin Zhong <devinzhong@outlook.com>
2017-02-06 22:17:05 +0800

    update script

--- shadowsocks/README.md
+++ shadowsocks/README.md
@@ -1,18 +1,21 @@
 # shadowsocks
 
-## shadowsocks-libev_debian.sh
+## shadowsocks-libev_server.sh
+### Required
+- Linux distribution with Systemd supported
+
 ### Feature
 - enable TCP fast open
 - enable udprelay mode
-- enable onetime authentication
 - start the server on boot
 - use chacha20 as encryption
 
 ### How to use
 ```bash
 su - root
-wget https://raw.githubusercontent.com/DevinZhong/scripts/master/shadowsocks/shadowsocks-libev_debian.sh
-bash shadowsocks-libev_debian.sh
+wget https://raw.githubusercontent.com/DevinZhong/scripts/master/shadowsocks/shadowsocks-libev_server.sh
+chmod +x shadowsocks-libev_server.sh
+./shadowsocks-libev_server.sh
 
 # recommend
 reboot
--- shadowsocks/shadowsocks-libev_server.sh
+++ shadowsocks/shadowsocks-libev_server.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
-#==================================================================
-#   System Required:  Debian 8 (32bit/64bit)
-#   Description:  Install Shadowsocks-libev for Debian 8
+#=========================================================================
+#   System Required:  Linux distribution with Systemd supported
+#   Description:  Install Shadowsocks-libev Server
 #   Author: Devin Zhong <devinzhong@outlook.com>
 #   Intro:  https://github.com/DevinZhong/scripts/tree/master/shadowsocks
-#==================================================================
+#=========================================================================
 
-# 清屏与打印脚本说明
+# 清屏
 clear
-cat << EOF
-#############################################################
-# Install Shadowsocks-libev for Debian 8 (32bit/64bit)
-# Intro:  https://github.com/DevinZhong/scripts/tree/master/shadowsocks
-#
-# Author: Devin Zhong <devinzhong@outlook.com>
-#############################################################
-
-EOF
 
 # 确保是 root 来运行此脚本
 if [[ $EUID -ne 0 ]]; then
@@ -24,87 +15,134 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-# 确保系统为 Debian 发行版
-if [ ! -f /etc/debian_version ]; then
-    echo "distribution is not supported!" 1>&2
-    exit 1
-fi
+# 提取发行版 ID
+distributor_id=$(lsb_release -i) && distributor_id=${distributor_id:16}
 
-# 更新包列表
-echo 'update packages list...'
-apt-get update > /dev/null
-
-# 安装 git
-echo 'install git...'
-apt-get -y install git > /dev/null
-
-# 安装 shadowsocks-libev
-echo 'install shadowsocks-libev...'
+# 生成并进入临时目录
 TEMP_DIR="shadowsocks-libev-$RANDOM"
 mkdir $TEMP_DIR && cd $TEMP_DIR
+
+# 更新软件包，并安装确保 git，gcc，make
+if [ $distributor_id = Debian -o $distributor_id = Ubuntu ]; then
+  apt update
+  apt upgrade -y
+  apt install -y git gcc make
+elif [ $distributor_id = CentOS -o $distributor_id = RedHatEnterpriseServer ]; then
+  yum upgrade -y
+  yum install -y git gcc make
+elif [ $distributor_id = Fedora ]; then
+  dnf upgrade -y
+  dnf install -y git gcc make
+elif [ $distributor_id = archlinux ]; then
+  pacman -Syu
+  pacman -S git gcc make
+fi
+
+# 编译安装最新的 mbedTLS 和 libsodium
+export LIBSODIUM_VER=1.0.11
+export MBEDTLS_VER=2.4.0
+wget https://download.libsodium.org/libsodium/releases/libsodium-$LIBSODIUM_VER.tar.gz
+tar xvf libsodium-$LIBSODIUM_VER.tar.gz
+pushd libsodium-$LIBSODIUM_VER
+./configure --prefix=/usr && make
+make install
+popd
+wget https://tls.mbed.org/download/mbedtls-$MBEDTLS_VER-gpl.tgz
+tar xvf mbedtls-$MBEDTLS_VER-gpl.tgz
+pushd mbedtls-$MBEDTLS_VER
+make SHARED=1 CFLAGS=-fPIC
+make install
+popd
+
+# 安装 shadowsocks-libev
 git clone https://github.com/shadowsocks/shadowsocks-libev.git &> /dev/null
 cd shadowsocks-libev
-apt-get -y install --no-install-recommends gettext build-essential autoconf libtool \
-    gawk debhelper dh-systemd init-system-helpers pkg-config asciidoc xmlto apg libpcre3-dev libmbedtls-dev \
-    libev-dev libudns-dev libsodium-dev &> /dev/null
-./autogen.sh && dpkg-buildpackage -b -us -uc -i &> /dev/null
+git submodule update --init --recursive
+if [ $distributor_id = Debian -o $distributor_id = Ubuntu ]; then
+  apt install -y --no-install-recommends gettext build-essential autoconf libtool libpcre3-dev asciidoc xmlto libev-dev libudns-dev automake
+elif [ $distributor_id = CentOS -o $distributor_id = RedHatEnterpriseServer ]; then
+  yum install -y gettext gcc autoconf libtool automake make asciidoc xmlto udns-devel libev-devel
+elif [ $distributor_id = Fedora ]; then
+  dnf install -y gettext gcc autoconf libtool automake make asciidoc xmlto udns-devel libev-devel
+elif [ $distributor_id = archlinux ]; then
+  pacman -S gettext gcc autoconf libtool automake make asciidoc xmlto udns libev
+fi
+./autogen.sh && ./configure && make
+make install
 cd ..
-dpkg -i shadowsocks-libev*.deb &> /dev/null
+
+# 销毁临时目录
 cd ..
 rm -rf $TEMP_DIR
 
-# 配置 shadowsocks-libev
-echo 'configure shadowsocks-libev...'
-# 启用 UDP 转发、一次性认证、TCP 快速启动
-sed -i 's/\(DAEMON_ARGS\).*$/\1="-u -A --fast-open"/' /etc/default/shadowsocks-libev
+# 编写 service 单元配置文件
+if [ ! -d /usr/lib/systemd/system ]; then
+  mkdir /usr/lib/systemd/system
+fi
+cat << "EOF" > /usr/lib/systemd/system/shadowsocks-libev.service
+[Unit]
+Description=Shadowsocks-libev Server Service
+After=network.target
 
-# 配置 config.json
+[Service]
+Type=simple
+User=root
+LimitNOFILE=32768
+ExecStart=/usr/local/bin/ss-server -c /etc/shadowsocks-libev/config.json -u --fast-open
+ExecReload=/bin/kill -HUP $MAINPID
+ExecStop=/bin/kill -QUIT $MAINPID
+PrivateTmp=true
+Restart=on-failure
+RestartSec=5s
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+
+# 获取 IP
+shadowsocksip=$(curl ifconfig.co)
+
+# 交互式配置 shadowsocks-libev
 echo "please input password (way2hacker.com):"
 read shadowsockspwd
 [ -z "${shadowsockspwd}" ] && shadowsockspwd="way2hacker.com"
-
 echo "please input port (8686):"
 read shadowsocksport
 [ -z "$shadowsocksport" ] && shadowsocksport="8686"
-
+mkdir /etc/shadowsocks-libev
 cat << EOF > /etc/shadowsocks-libev/config.json
 {
-    "server":"0.0.0.0",
-    "server_port":${shadowsocksport},
-    "local_port":1080,
-    "password":"${shadowsockspwd}",
-    "timeout":60,
-    "method":"chacha20"
+  "server":"${shadowsocksip}",
+  "server_port":${shadowsocksport},
+  "local_port":1080,
+  "password":"${shadowsockspwd}",
+  "timeout":60,
+  "method":"chacha20"
 }
 EOF
 
 # 开启服务器 TCP Fast Open 支持
-echo 'enable TCP Fast Open on server...'
 echo "net.ipv4.tcp_fastopen = 3" >> /etc/sysctl.conf
 sysctl -p > /dev/null
 
-# 重启服务
-echo 'restart shadowsocks-libev...'
-sudo systemctl daemon-reload
-sudo systemctl restart shadowsocks-libev.service
+# 启动服务
+systemctl enable shadowsocks-libev.service
+systemctl start shadowsocks-libev.service
 
 # 搭建完成
-echo 'Complete!'
 cat << EOF
+Complete!
 
 please configure you shadowsocks client as below:
-----------------------------------------
-server:"$(curl ifconfig.co 2>/dev/null)"
+------------------------------------------------
+server:"${shadowsocksip}"
 server_port:${shadowsocksport}
 password:"${shadowsockspwd}"
 method:"chacha20"
-auth:true
 fast_open:true
-----------------------------------------
-- remember to add the '-u' argument to the ss-local to enable the udprelay mode.
-- your system must support TCP fast open if you want to enjoy this feature.
-
+------------------------------------------------
 EOF
-echo 'reboot your server to get more free memory!'
 
+# 脚本结束
 exit 0
