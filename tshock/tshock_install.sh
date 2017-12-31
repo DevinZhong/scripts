713f9c4500690a4b0ede08a1eba1f416286db872
xiaoshi <xiaoshi@imagedt.com>
2017-12-31 21:05:32 +0800

    update tshock script

--- tshock/tshock_install.sh
+++ tshock/tshock_install.sh
@@ -15,26 +15,27 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-echo "#### upgrade packages..."
+echo "\n#### upgrade packages..."
 yum upgrade -y
 
-echo "#### install mono..."
+echo "\n#### install mono..."
 yum install -y yum-utils
 rpm --import "http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
 yum-config-manager --add-repo http://download.mono-project.com/repo/centos7/
 yum install -y mono-devel mono-complete
 
-echo "#### download tshock..."
+echo "\n#### download tshock..."
 wget -O tshock.zip 'https://github.com/Pryaxis/TShock/releases/download/v4.3.25/tshock_4.3.25.zip'
 
-echo "#### unzip tshock..."
+echo "\n#### unzip tshock..."
 unzip tshock.zip -d ~/tshock
 
-echo "#### install tmux..."
+echo "\n#### install tmux..."
 yum install -y tmux
 
 # 搭建完成
 cat << EOF
+
 #### Complete!
 
 You can run the TShock in tmux session.
